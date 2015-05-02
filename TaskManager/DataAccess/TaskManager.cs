6c31638538bd20d9d0b66678d36239cf102bab0e
Jan Graefe <jan.graefe@gmx.de>
2015-05-02 18:55:55 +0200

    added taskmanager

---
+++ TaskManager/DataAccess/TaskManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ch.jaxx.TaskManager.DataAccess
+{
+    public class TaskManager
+    {
+        private string connectionString;
+        private FirebirdContext context;
+
+        /// <summary>
+        /// Creates a new instance of TaskManager
+        /// </summary>
+        /// <param name="ConnectionString"></param>
+        public TaskManager(string ConnectionString)
+        {
+            this.connectionString = ConnectionString;
+            this.context = new FirebirdContext(this.connectionString);
+        }
+
+        /// <summary>
+        /// Adds a new task at the end of the task queue
+        /// </summary>
+        /// <param name="TaskToQueue"></param>
+        public void CreateQueueTask(TaskModel TaskToQueue)
+        {
+            context.Tasks.Add(TaskToQueue);
+            context.SaveChanges();
+            
+        }
+
+        public List<TaskModel> GetAllTasks()
+        {
+            return context.Tasks.ToList();            
+        }
+    }
+}
