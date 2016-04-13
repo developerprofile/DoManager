f651f37612e9a808b5052ce2a38fd3f67c9def07
Viper3400 <viper3400@gmx.de>
2016-04-13 13:51:39 +0200

    Migrated to Autofac (way to #2)

--- DoManagerMui/ViewModel/ViewModelLocator.cs
+++ DoManagerMui/ViewModel/ViewModelLocator.cs
@@ -12,6 +12,7 @@
   See http://www.galasoft.ch/mvvm
 */
 
+using Autofac;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
@@ -24,12 +25,16 @@ namespace DoManagerMui.ViewModel
     /// </summary>
     public class ViewModelLocator
     {
+        private IContainer viewContainer;
+
         /// <summary>
         /// Initializes a new instance of the ViewModelLocator class.
         /// </summary>
         public ViewModelLocator()
         {
-            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MainViewModel>();
+            viewContainer = builder.Build();
 
             ////if (ViewModelBase.IsInDesignModeStatic)
             ////{
@@ -42,14 +47,17 @@ namespace DoManagerMui.ViewModel
             ////    SimpleIoc.Default.Register<IDataService, DataService>();
             ////}
 
-            SimpleIoc.Default.Register<MainViewModel>();
+          
         }
 
         public MainViewModel Main
         {
             get
             {
-                return ServiceLocator.Current.GetInstance<MainViewModel>();
+                using (var scope = viewContainer.BeginLifetimeScope())
+                {
+                    return scope.Resolve<MainViewModel>();
+                }
             }
         }
         
