8f8881c6a86d2874fa7ac25440ff476409022d87
viper3400 <viper_3400@gmx.de>
2017-04-17 17:49:27 +0200

    replaced state text with colored  state labels (closes #13)

--- DoManagerMui/ViewModel/TaskViewModel.cs
+++ DoManagerMui/ViewModel/TaskViewModel.cs
@@ -22,22 +22,27 @@ namespace DoManagerMui.ViewModel
                     case TaskState.ACTIVE:
                         meta.State = Task.State.ToString();
                         meta.Color = Color.OliveDrab.Name;
+                        meta.TextColor = Color.White.Name;
                         break;
                     case TaskState.BLOCKED:
                         meta.State = Task.State.ToString();
-                        meta.Color = Color.Gray.Name;
+                        meta.Color = Color.Silver.Name;
+                        meta.TextColor = Color.SaddleBrown.Name;
                         break;
                     case TaskState.DONE:
                         meta.State = Task.State.ToString();
                         meta.Color = Color.Gray.Name;
+                        meta.TextColor = Color.White.Name;
                         break;
                     case TaskState.NEXT:
                         meta.State = Task.State.ToString();
                         meta.Color = Color.DarkOrange.Name;
+                        meta.TextColor = Color.SaddleBrown.Name;
                         break;
                     default:
                         meta.State = "TO DO";
-                        meta.Color = Color.LightSalmon.Name;
+                        meta.Color = Color.Moccasin.Name;
+                        meta.TextColor = Color.SaddleBrown.Name;
                         break;
                 }
 
@@ -45,12 +50,13 @@ namespace DoManagerMui.ViewModel
             }
         }
 
-       
+
     }
 
     public class TaskMetaData
     {
         public string Color { get; set; }
+        public string TextColor { get; set; }
         public string State { get; set; }
     }
 }
